bc2d9d1ab4dfa963d1c0e2e410419a6600ff2216
CZX93-BW <yunimoon2012@gmail.com>
2026-06-12 19:44:44 +0200

    feat(main-page): compose initial page sections

--- src/app/pages/main-page/main-page.ts
+++ src/app/pages/main-page/main-page.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { AboutMe } from '../../sections/about-me/about-me';
+import { Contact } from '../../sections/contact/contact';
+import { FeaturedProjects } from '../../sections/featured-projects/featured-projects';
+import { Hero } from '../../sections/hero/hero';
+import { SkillSet } from '../../sections/skill-set/skill-set';
+import { Testimonials } from '../../sections/testimonials/testimonials';
 
 @Component({
   selector: 'app-main-page',
-  imports: [],
+  imports: [
+    Hero,
+    AboutMe,
+    SkillSet,
+    FeaturedProjects,
+    Testimonials,
+    Contact,
+  ],
   templateUrl: './main-page.html',
   styleUrl: './main-page.scss',
 })
-export class MainPage {}
+export class MainPageComponent {}
