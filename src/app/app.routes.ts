70cf7b39e1bcde21e22b9074d86232fb4dad184c
CZX93-BW <yunimoon2012@gmail.com>
2026-06-12 19:43:48 +0200

    feat(routing): add main and legal notice routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { LegalNoticeComponent } from './pages/legal-notice/legal-notice';
+import { MainPageComponent } from './pages/main-page/main-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainPageComponent,
+    title: 'Portfolio | Bastian Wollny',
+  },
+  {
+    path: 'legal-notice',
+    component: LegalNoticeComponent,
+    title: 'Legal Notice | Bastian Wollny',
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
