99daa41751e8d820f3fda78e9ba178f309b17854
CZX93-BW <yunimoon2012@gmail.com>
2026-06-12 12:48:44 +0200

    feat(core): setup initial portfolio project structure

---
+++ src/app/features/project-dialog/project-dialog.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-dialog',
+  imports: [],
+  templateUrl: './project-dialog.html',
+  styleUrl: './project-dialog.scss',
+})
+export class ProjectDialog {}
