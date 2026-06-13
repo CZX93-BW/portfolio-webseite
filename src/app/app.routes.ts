a2dcf882277f18febbee478f743ab12bcc2b2b10
CZX93-BW <yunimoon2012@gmail.com>
2026-06-13 19:50:17 +0200

    feat(legal): add privacy policy route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LegalNoticeComponent } from './pages/legal-notice/legal-notice';
 import { MainPageComponent } from './pages/main-page/main-page';
+import { PrivacyPolicyComponent } from './pages/privacy-policy/privacy-policy';
 
 export const routes: Routes = [
   {
@@ -12,6 +13,11 @@ export const routes: Routes = [
     path: 'legal-notice',
     component: LegalNoticeComponent,
     title: 'Legal Notice | Bastian Wollny',
+  },
+  {
+    path: 'privacy-policy',
+    component: PrivacyPolicyComponent,
+    title: 'Privacy Policy | Bastian Wollny',
   },
   {
     path: '**',
