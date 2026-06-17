d09ecc51138ae58ec34bbbab48a6f00ede3daa7a
CZX93-BW <yunimoon2012@gmail.com>
2026-06-17 23:58:41 +0200

    feat(main): add testimonials section

--- src/app/pages/main-page/main-page.ts
+++ src/app/pages/main-page/main-page.ts
@@ -4,7 +4,7 @@ import { Contact } from '../../sections/contact/contact';
 import { FeaturedProjectsComponent } from '../../sections/featured-projects/featured-projects';
 import { Hero } from '../../sections/hero/hero';
 import { SkillSet } from '../../sections/skill-set/skill-set';
-import { Testimonials } from '../../sections/testimonials/testimonials';
+import { TestimonialsComponent } from '../../sections/testimonials/testimonials';
 
 @Component({
   selector: 'app-main-page',
@@ -13,10 +13,10 @@ import { Testimonials } from '../../sections/testimonials/testimonials';
     AboutMe,
     SkillSet,
     FeaturedProjectsComponent,
-    Testimonials,
+    TestimonialsComponent,
     Contact,
   ],
   templateUrl: './main-page.html',
-  styleUrl: './main-page.scss',
+  styleUrls: ['./main-page.scss'],
 })
 export class MainPageComponent {}
