e3daf6ccbe64515f4a26a83c8a7180488f932703
CZX93-BW <yunimoon2012@gmail.com>
2026-06-18 23:00:43 +0200

    feat(main): add contact section

--- src/app/pages/main-page/main-page.ts
+++ src/app/pages/main-page/main-page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { AboutMe } from '../../sections/about-me/about-me';
-import { Contact } from '../../sections/contact/contact';
+import { ContactComponent } from '../../sections/contact/contact';
 import { FeaturedProjectsComponent } from '../../sections/featured-projects/featured-projects';
 import { Hero } from '../../sections/hero/hero';
 import { SkillSet } from '../../sections/skill-set/skill-set';
@@ -14,7 +14,7 @@ import { TestimonialsComponent } from '../../sections/testimonials/testimonials'
     SkillSet,
     FeaturedProjectsComponent,
     TestimonialsComponent,
-    Contact,
+    ContactComponent,
   ],
   templateUrl: './main-page.html',
   styleUrls: ['./main-page.scss'],
