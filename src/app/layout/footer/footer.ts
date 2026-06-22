7888b2bec7a3f27efadca15396c4e0692f568bb7
CZX93-BW <yunimoon2012@gmail.com>
2026-06-22 02:26:48 +0200

    fixfooter): fixed final global footer layout & design

--- src/app/layout/footer/footer.ts
+++ src/app/layout/footer/footer.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
+interface FooterLink {
+  label: string;
+  href?: string;
+  routerLink?: string;
+}
+
 @Component({
   selector: 'app-footer',
   imports: [RouterLink],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
-export class Footer {}
+export class Footer {
+  protected readonly footerLinks: FooterLink[] = [
+    { label: 'Github', href: 'https://github.com/CZX93-BW' },
+    { label: 'LinkedIn', href: 'https://www.linkedin.com/in/bastian-wollny' },
+    { label: 'Email', href: 'mailto:bastian-wollny@web.de' },
+    { label: 'Legal Notice', routerLink: '/legal-notice' },
+    { label: 'Privacy Policy', routerLink: '/privacy-policy' },
+  ];
+}
